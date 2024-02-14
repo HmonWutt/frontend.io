c5aabf45b77808d7ee8ced47a8a90bc7c50b3f88
hmon <wu2504hm-s@student.lu.se>
2024-02-14 19:15:31 +0100

    fixed spelling mistake

--- assets/quotes.js
+++ assets/quotes.js
@@ -2,7 +2,7 @@ const quotes = {
   quotes: [
     "To create an environment in which knowledge about coffee and its sphere can be obtained",
 
-    "This is a great introduction to the coffee industry's best beans on tne planet",
+    "This is a great introduction to the coffee industry's best beans on the planet",
 
     "Coffee inside out, this is the way of eternal life and empowerment of the soul",
   ],
