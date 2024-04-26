630de5581d316b25a6436e71fb79091e4990bada
hmon <wtthumon@gmail.com>
2024-04-26 16:04:53 +0200

    Deleted unnecessary code

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,4 @@
-// window.onload = function () {
 
-//     var el = document.createElement("div");
-//     el.innerHTML = "RABBIT TEST ";
-//     var div = document.getElementById("headline");
-//     insertAfter(div, el);
-
-//     /* FUNCTIONS */
-
-//     // Inserts newNode after referenceNode
-//     function insertAfter(referenceNode, newNode) {
-//         referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
-//     }
-// };
 import articles from "../assets/articles.js";
 import quotes from "../assets/quotes.js";
 
